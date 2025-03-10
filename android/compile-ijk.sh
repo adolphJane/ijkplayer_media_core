a3a52015d00e73097ea7ef99d25249da7aefddeb
jiang <jrm01911545@taobao.com>
2025-03-10 15:34:32 +0800

    update：更新代码

--- android/compile-ijk.sh
+++ android/compile-ijk.sh
@@ -16,12 +16,14 @@
 # limitations under the License.
 #
 
+#环境变量检查
 if [ -z "$ANDROID_NDK" -o -z "$ANDROID_NDK" ]; then
     echo "You must define ANDROID_NDK, ANDROID_SDK before starting."
     echo "They must point to your NDK and SDK directories.\n"
     exit 1
 fi
 
+#变量定义
 REQUEST_TARGET=$1
 REQUEST_SUB_CMD=$2
 ACT_ABI_32="armv5 armv7a"
@@ -29,6 +31,7 @@ ACT_ABI_64="armv5 armv7a arm64"
 ACT_ABI_ALL=$ACT_ABI_64
 UNAME_S=$(uname -s)
 
+#并行编译标志
 FF_MAKEFLAGS=
 if which nproc >/dev/null
 then
@@ -38,6 +41,7 @@ then
     FF_MAKEFLAGS=-j`sysctl -n machdep.cpu.thread_count`
 fi
 
+#子命令处理函数
 do_sub_cmd () {
     SUB_CMD=$1
     if [ -L "./android-ndk-prof" ]; then
@@ -69,6 +73,8 @@ do_sub_cmd () {
     esac
 }
 
+#NDK 编译处理函数
+#根据目标架构切换到相应的目录并调用 do_sub_cmd 函数执行子命令。
 do_ndk_build () {
     PARAM_TARGET=$1
     PARAM_SUB_CMD=$2
@@ -87,7 +93,7 @@ do_ndk_build () {
     esac
 }
 
-
+#主逻辑
 case "$REQUEST_TARGET" in
     "")
         do_ndk_build armv7a;
